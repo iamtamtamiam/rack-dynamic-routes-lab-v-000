6658e49b931d8174192a2594aeef6f5da4567f27
Tam Vu <vutam_a@hotmail.com>
2019-10-27 19:23:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,14 +5,12 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
-      @@items.each do |item|
         if @@items.find{|all_item| all_item.name == item}
           resp.write "#{all_item}"
         elsif 
           resp.write "Item not found."
           resp.status = 400
         end 
-      end
     else
       resp.write "Item not found. Route not found."
       resp.status = 404
